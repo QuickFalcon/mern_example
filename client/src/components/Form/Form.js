115d628caaf810471c501d79a55b5368c729c9db
Francisco Sosa <sosa3500@hotmail.com>
2022-03-22 10:47:28 -0600

    Setting page skeleton

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Form = () => {
+    return <h2>FORM</h2>
+}
+
+export default Form
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Post from './Post/Post'
+
+const Posts = () => {
+    return (
+        <>
+            <h2>POSTS</h2>
+            <Post />
+            <Post />
+        </>
+    )
+}
+
+export default Posts
