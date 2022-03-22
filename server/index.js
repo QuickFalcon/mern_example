21d0d334e8d573887978b89d88a2ff2f3bfcd6ea
Francisco Sosa <sosa3500@hotmail.com>
2022-03-22 08:36:54 -0600

    Setting up mongoose

--- server/index.js
+++ server/index.js
@@ -2,3 +2,25 @@ import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
+
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://paco:Pakilu1016.@cluster0.ylsxb.mongodb.net/mern?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    app.listen(PORT, () => {
+        console.log('Server running')
+    })
+})
+.catch((error) => {
+    console.log(error.message)
+})
